8e3d59ceaac0de13556282f8afc7ddcc225089b5
Linus Kendall <git@linuskendall.com>
2016-06-03 18:22:43 +0000

    Added continuous service discvoery task definition

--- update.sh
+++ update.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
 aws ecs register-task-definition --cli-input-json file://$(pwd)/task-definition.json
+aws ecs register-task-definition --cli-input-json file://$(pwd)/task-definition-continuous.json
 aws ecs update-service --service service-discovery --task-definition service-discovery --cluster development
