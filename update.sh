b5e9ca8b57c0a7d34107a51f609766455b3eda08
Linus Kendall <git@linuskendall.com>
2016-08-20 17:06:48 +0000

    Added service discovery for development workers

--- update.sh
+++ update.sh
@@ -3,3 +3,4 @@
 aws ecs register-task-definition --cli-input-json file://$(pwd)/task-definition.json
 aws ecs register-task-definition --cli-input-json file://$(pwd)/task-definition-continuous.json
 aws ecs update-service --service service-discovery --task-definition service-discovery --cluster development
+aws ecs update-service --service service-discovery --task-definition service-discovery --cluster development-workers
