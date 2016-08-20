3c234a060377586f30460e458e092a3a3db6ddf0
Linus Kendall <git@linuskendall.com>
2016-08-20 17:23:47 +0000

    Added script to launch service discovery tasks

--- update.sh
+++ update.sh
@@ -4,3 +4,5 @@ aws ecs register-task-definition --cli-input-json file://$(pwd)/task-definition.
 aws ecs register-task-definition --cli-input-json file://$(pwd)/task-definition-continuous.json
 aws ecs update-service --service service-discovery --task-definition service-discovery --cluster development
 aws ecs update-service --service service-discovery --task-definition service-discovery --cluster development-workers
+aws ecs update-service --service service-discovery-continuous --task-definition service-discovery-continuous --cluster development --desired-count 1
+aws ecs update-service --service service-discovery-continuous --task-definition service-discovery-continuous --cluster development-workers --desired-count 1
